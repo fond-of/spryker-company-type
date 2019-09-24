be3cc4f0e004273eab39ffbf9dded008e05282dd
Jozsef Geng <gengjozsef86@gmail.com>
2019-09-24 16:28:30 +0200

    add company-type-manufacturer

--- src/FondOfSpryker/Shared/CompanyType/CompanyTypeConstants.php
+++ src/FondOfSpryker/Shared/CompanyType/CompanyTypeConstants.php
@@ -5,4 +5,5 @@ namespace FondOfSpryker\Shared\CompanyType;
 interface CompanyTypeConstants
 {
     public const DEFAULT_COMPANY_TYPE_NAME = 'DEFAULT_COMPANY_TYPE_NAME';
+    public const MANUFACTURER_COMPANY_TYPE_NAME = 'MANUFACTURER_COMPANY_TYPE_NAME';
 }
--- src/FondOfSpryker/Zed/CompanyType/Business/CompanyTypeFacade.php
+++ src/FondOfSpryker/Zed/CompanyType/Business/CompanyTypeFacade.php
@@ -2,6 +2,7 @@
 
 namespace FondOfSpryker\Zed\CompanyType\Business;
 
+use Generated\Shared\Transfer\CompanyCollectionTransfer;
 use Generated\Shared\Transfer\CompanyResponseTransfer;
 use Generated\Shared\Transfer\CompanyTypeCollectionTransfer;
 use Generated\Shared\Transfer\CompanyTypeTransfer;
@@ -17,11 +18,11 @@ class CompanyTypeFacade extends AbstractFacade implements CompanyTypeFacadeInter
     /**
      * {@inheritdoc}
      *
-     * @api
-     *
      * @param \Generated\Shared\Transfer\CompanyTypeTransfer $companyTypeTransfer
      *
      * @return \Generated\Shared\Transfer\CompanyTypeTransfer|null
+     * @api
+     *
      */
     public function getCompanyTypeById(CompanyTypeTransfer $companyTypeTransfer): ?CompanyTypeTransfer
     {
@@ -31,9 +32,9 @@ class CompanyTypeFacade extends AbstractFacade implements CompanyTypeFacadeInter
     /**
      * {@inheritdoc}
      *
+     * @return \Generated\Shared\Transfer\CompanyTypeCollectionTransfer
      * @api
      *
-     * @return \Generated\Shared\Transfer\CompanyTypeCollectionTransfer
      */
     public function getCompanyTypes(): CompanyTypeCollectionTransfer
     {
@@ -67,15 +68,53 @@ class CompanyTypeFacade extends AbstractFacade implements CompanyTypeFacadeInter
     /**
      * {@inheritdoc}
      *
-     * @api
-     *
      * @param \Generated\Shared\Transfer\CompanyTypeTransfer $companyTypeTransfer
      *
      * @return void
+     * @api
+     *
      */
     public function deleteCompanyType(CompanyTypeTransfer $companyTypeTransfer): void
     {
         $this->getFactory()->createCompanyTypeWriter()->deleteById($companyTypeTransfer);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param int $idCompanyType
+     *
+     * @return \Generated\Shared\Transfer\CompanyTypeTransfer|null
+     * @api
+     *
+     */
+    public function findCompanyTypeById(int $idCompanyType): ?CompanyTypeTransfer
+    {
+        return $this->getRepository()->getById($idCompanyType);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array $companyTypeIds
+     *
+     * @return \Generated\Shared\Transfer\CompanyCollectionTransfer|null
+     */
+    public function findCompaniesByCompanyTypeIds(array $companyTypeIds): ?CompanyCollectionTransfer
+    {
+        return $this->getRepository()->findCompaniesByCompanyTypeIds($companyTypeIds);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param \Generated\Shared\Transfer\CompanyTypeTransfer $companyTypeTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyTypeTransfer|null
+     */
+    public function getCompanyTypeByName(CompanyTypeTransfer $companyTypeTransfer): ?CompanyTypeTransfer
+    {
+        return $this->getFactory()->createCompanyTypeReader()->getByName($companyTypeTransfer);
     }
 
     /**
@@ -85,10 +124,18 @@ class CompanyTypeFacade extends AbstractFacade implements CompanyTypeFacadeInter
      *
      * @throws
      */
-    public function assignDefaultCompanyTypeToNewCompany(CompanyResponseTransfer $companyResponseTransfer): CompanyResponseTransfer
-    {
+    public function assignDefaultCompanyTypeToNewCompany(CompanyResponseTransfer $companyResponseTransfer
+    ): CompanyResponseTransfer {
         return $this->getFactory()
             ->createCompanyTypeAssigner()
             ->assignDefaultCompanyTypeToNewCompany($companyResponseTransfer);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCompanyTypeManufacturerName(): ?string
+    {
+        return $this->getFactory()->getConfig()->getCompanyTypeManufacturer();
     }
 }
